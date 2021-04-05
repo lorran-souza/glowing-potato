358df81b15f88c478cc2b288401fff71b71e25d9
lorran-souza <lorransouzamch@gmail.com>
2021-04-05 13:33:59 -0300

    Atualizando Cliente_Controller

--- controllers/cliente_controller.py
+++ controllers/cliente_controller.py
@@ -9,15 +9,47 @@ class ClienteController():
 
     def run(self):
         opcao = self.__cliente_view.tela_principal()
+        while opcao != "0":
+            if opcao == "1":
+                self.cadastra()
+            elif opcao == "2":
+                self.lista()
+            elif opcao == "3":
+                self.atualiza()
+            elif opcao == "4":
+                self.remove()
+            opcao = self.__view.tela_principal()
 
-    def adiciona(self):
-        pass
+    def cadastra(self):
+        info = self.__view.cadastra()
+
+        if info is not None:
+            for cliente in self.__concessionaria.clientes:
+                if cliente.num_id == info[2]:
+                    self.__view.erro()
+                    return
+
+            cliente = Cliente(info[0], info[1], info[2])
+            self.__concessionaria.cadastra_objeto(cliente)
+            self.__view.sucesso()
 
     def lista(self):
-        pass
+        self.__view.lista(self.__concessionaria.clientes)
 
     def atualiza(self):
-        pass
+        info = self.__view.atualiza()
+
+        if info is not None:
+            for cliente in self.__concessionaria.clientes:
+                if cliente.num_id == info[2]:
+                    cliente.nome = info[0]
+                    cliente.telefone = info[1]
 
     def remove(self):
-        pass
+        num_id = self.__view.remove()
+        for cliente in self.__concessionaria.clientes:
+            if cliente.num_id == num_id:
+                self.__concessionaria.remove_objeto(cliente)
+
+
+    
